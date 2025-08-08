99b1942076d710ae9988bda0a9efbca6a0e32027
Shiva Kumar Reddy Rangapuram <162505208+rskr6959@users.noreply.github.com>
2025-08-07 22:27:45 -0700

    Time: 0 ms (100%), Space: 41.6 MB (98.92%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        if (nums.length == 1) return nums[0] == target ? 0 : -1;
+
+        int start = 0;
+        int end = nums.length - 1;
+        String region;
+
+        if (target == nums[end]) {
+            return end;
+        } else if (target < nums[end]) {
+            region = "orange"; 
+        } else {
+            region = "green";  
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) return mid;
+            if (nums[mid] <= nums[nums.length - 1] && region.equals("green")) {
+                end = mid - 1;
+            } else if (nums[mid] > nums[nums.length - 1] && region.equals("orange")) {
+                start = mid + 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
