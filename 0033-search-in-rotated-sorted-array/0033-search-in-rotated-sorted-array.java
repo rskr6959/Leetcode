fa2502bc337d6a6de96a60dc09ad0811c8d6f24e
Shiva Kumar Reddy Rangapuram <162505208+rskr6959@users.noreply.github.com>
2025-08-22 12:10:32 -0700

    Time: 0 ms (100%), Space: 42.3 MB (29.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        if (nums.length == 1) return nums[0] == target ? 0 : -1;
-
+        if(nums.length == 0) return -1;
+        if(nums.length == 1) 
+        return (nums[0] == target) ? 0 : -1;
         int start = 0;
         int end = nums.length - 1;
         String region;
-
-        if (target == nums[end]) {
-            return end;
-        } else if (target < nums[end]) {
-            region = "orange"; 
-        } else {
-            region = "green";  
-        }
-
-        while (start <= end) {
+        if(target == nums[end])
+        return end;
+        else if(target > nums[end])
+        region = "green";
+        else
+        region = "orange";
+        while(start <= end){
             int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) return mid;
-            if (nums[mid] <= nums[nums.length - 1] && region.equals("green")) {
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(nums[mid] > nums[nums.length - 1] && region.equals("orange")){
+                start = mid + 1;
+            }
+            else if(nums[mid] <= nums[nums.length - 1] && region.equals("green")){
                 end = mid - 1;
-            } else if (nums[mid] > nums[nums.length - 1] && region.equals("orange")) {
+            }
+            else if(nums[mid] < target){
                 start = mid + 1;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-
-            } else {
+            }
+            else{
                 end = mid - 1;
             }
         }
-
-        return -1;
+    return -1;
     }
 }
