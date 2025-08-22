eb833239b22fc4cb2f71516bb009dfb76ce9e997
Shiva Kumar Reddy Rangapuram <162505208+rskr6959@users.noreply.github.com>
2025-08-21 20:26:36 -0700

    Time: 16 ms (19.52%), Space: 48.9 MB (44.72%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums.length == k)
+        return nums;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int num : nums){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+        Queue<Integer> minHeap = new PriorityQueue<>((a, b) -> freqMap.get(a) - freqMap.get(b));
+        for(int num : freqMap.keySet()){
+            minHeap.add(num);
+            if(minHeap.size() > k)
+            minHeap.poll();
+        }
+        int[] result = new int[minHeap.size()];
+        for(int i = 0; i < k; i++){
+            result[i] = minHeap.poll();
+        }
+    return result;
+    }
+}
